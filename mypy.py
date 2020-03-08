22a7299d5b22420336079594f9021343bf516852
Clark <franzelclark@gmail.com>
2020-03-08 10:00:23 +0800

    Add files via upload
    added mypy.py

---
+++ mypy.py
@@ -0,0 +1,19 @@
+__author__ = 'clark ronquillo ust-computerscience'
+
+from sklearn import tree
+#features = weight, 0 for bumpy 1 for smooth
+#labels = 0 for apples 1 for oranges
+
+features = [[140, 1], [130, 1], [150, 0], [170, 0]]
+labels = [0, 0, 1, 1]
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features, labels)
+
+prediction = clf.predict([[int(input("Weight: ")), int(input("Smoothness: "))]])
+if prediction == "[0]"
+	print ("Apple")
+elif prediction == "[1]"
+	print ("Orange")
+
+print(prediction)
+#[0] or [1]
